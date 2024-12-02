63f6924509ac6b89e66a78965c29b4346147f4ad
Jackson Tam <Jackson.Tam@OOCL.com>
2024-12-02 11:59:16 +0800

    feat: add setter and getter for todo model

--- src/main/java/org/example/day11todobackend/model/Todo.java
+++ src/main/java/org/example/day11todobackend/model/Todo.java
@@ -16,4 +16,27 @@ public class Todo {
 
     private boolean done;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
